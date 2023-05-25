ce270e4d32073cbe43450f7c1278448a95cb322f
Abel-libery <125762174+Abel-libery@users.noreply.github.com>
2023-05-24 17:13:27 -1200

    Add files via upload
    offsite calendar code nav side bar

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React, {useState} from 'react';
+import Navbar from "./components/Navbar";
+import { BrowserRouter, Routes, Route,Navigate } from "react-router-dom";
+import "./style/index.css";
+
+function App() {
+	const [navVisible, showNavbar] = useState(false);
+
+	return (
+		<BrowserRouter >
+			<div className="App">
+				<Navbar visible={ navVisible } show={ showNavbar } />
+				<Routes>
+					<Route path="/" element={<Navigate to="/dashboard" />} />
+					<Route path='/dashboard' element={
+						<div className={!navVisible ? "page" : "page page-with-navbar"}>
+							<h1>Dashboard</h1>
+							{/* <div className="contentbox">
+
+							</div> */}
+						</div>
+					} />
+					<Route path='/analytics' element={
+						<div className={!navVisible ? "page" : "page page-with-navbar"}>
+							<h1>Share point</h1>
+						</div>
+					}/>
+					<Route path='/orders' element={
+						<div className={!navVisible ? "page" : "page page-with-navbar"}>
+							<h1>Report</h1>
+						</div>
+					}/>
+					<Route path='/settings' element={
+						<div className={!navVisible ? "page" : "page page-with-navbar"}>
+							<h1>Settings</h1>
+						</div>
+					}/>
+				</Routes>
+			</div>
+		</BrowserRouter>
+  );
+}
+
+export default App;
